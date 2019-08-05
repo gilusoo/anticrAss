8f3a696700d0ce79461dd11dce4dbc135323d68a
gilusoo <melg@LAPTOP-VUD46U1R.localdomain>
2019-08-05 07:35:11 -0700

    Debugging

--- bin/sra_scraper.py
+++ bin/sra_scraper.py
@@ -1,5 +1,5 @@
 """
-Reads in a text file containing a list of SRA accessions, then goes through one run at a time
+Reads in a text file containing a list of SRA accessions. Then goes through one run at a time
 to collect the available metadata and writes the desired fields to a new file.
 """
 
@@ -11,14 +11,14 @@ import argparse
 
 def sra_acc(infile):
     acc_list = []
-    with open(infile, 'r'):
+    with open(infile, 'r') as infile:
         for line in infile:
             line = line.strip()
             acc_list.append(line)
     return acc_list
 
-def pull_metadata(list, outfile, *args):
-    header = ['Run', args]
+def pull_metadata(list, outfile, att_list):
+    header = 'Run,{}'.format(','.join(att_list))
     outlines = []
     for run in list:
         req = requests.get('https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?run={}'.format(run))
@@ -28,18 +28,16 @@ def pull_metadata(list, outfile, *args):
         all_att_values = line.find_all('td')
         attributes_dict = {}
         for idx, att in enumerate(all_attributes):
-            attributes_dict[att] = all_att_values[idx].contents[0]
-        currline = 'Run'
-        for item in args:
+            attributes_dict[att.contents[0]] = all_att_values[idx].contents[0]
+        currline = run
+        for item in att_list:
             data = attributes_dict.get(item, 'N/A')
             currline = currline + ',' + data
         outlines.append(currline)
-        currline = 'Run'
-    with open(outfile, 'w'):
-        for item in header:
-            outfile.write('{},'.format(item))
+    with open(outfile, 'w') as outfile:
+        outfile.write(header)
         outfile.write('\n')
-        for line in currline:
+        for line in outlines:
             outfile.write('{}\n'.format(line))
     return
 
@@ -48,8 +46,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-i', '--input', help='Input SRA accession list')
     parser.add_argument('-o', '--output', help='Output csv file to write to')
-    parser.add_argument('-a', '--attributes', nargs='+', help='List of desired attributes from each run; '
-                                                              'separated by a single space')
+    parser.add_argument('-a', '--attributes', nargs='+', help='List of desired attributes from each run;'
+                                                              'format: \'[<att1>,<att2>,...])\'')
 
     try:
         args = parser.parse_args()
@@ -57,6 +55,5 @@ if __name__ == '__main__':
         parser.print_help()
         sys.exit(1)
 
-
-acclist = sra_acc(args.input)
-pull_metadata(acclist, args.output, args.attributes)
+    acclist = sra_acc(args.input)
+    pull_metadata(acclist, args.output, args.attributes)

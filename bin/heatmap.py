3e68cfca9b5ba80ba1b449957032eab54f03bc5f
gilusoo <melissagiluso@gmail.com>
2020-03-06 15:45:07 -0800

    minor fixes to logarithmic method

--- bin/heatmap.py
+++ bin/heatmap.py
@@ -175,14 +175,19 @@ if __name__ == '__main__':
         breaks = hm.heatmap_df(hm.hits_dict(args.data, hm.get_contigs(args.fasta)), hm.get_contigs(args.fasta),
                              hm.group_runs('sra_annotations.tsv'), of_interest)[1]
 
+        heat = sns.heatmap(data, cmap='inferno', vmin=0, vmax=100)
+        plt.show(block=True)
+        plt.hlines(breaks, xmin=0, xmax=len(hm.get_contigs(args.fasta)), colors='w')
+        plt.savefig(args.output)
+
     elif args.version =='log':
         data = hm.heatmap_df(hm.log_hits_dict(args.data, hm.get_contigs(args.fasta)), hm.get_contigs(args.fasta),
                              hm.group_runs('sra_annotations.tsv'), of_interest)[0]
         breaks = hm.heatmap_df(hm.log_hits_dict(args.data, hm.get_contigs(args.fasta)), hm.get_contigs(args.fasta),
                                hm.group_runs('sra_annotations.tsv'), of_interest)[1]
 
-    heat = sns.heatmap(data, cmap='inferno')
-    plt.show(block=True)
-    plt.hlines(breaks, xmin=0, xmax=len(hm.get_contigs(args.fasta)), colors='w')
-    plt.savefig(args.output)
+        heat = sns.heatmap(data, cmap='inferno', vmin=0, vmax=2.0)
+        plt.show(block=True)
+        plt.hlines(breaks, xmin=0, xmax=len(hm.get_contigs(args.fasta)), colors='w')
+        plt.savefig(args.output)
 
